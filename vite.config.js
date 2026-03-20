96d5987d988922f0f05143eac536351ff9202041
nate merkl <nattyesquire@gmail.com>
2026-03-20 12:28:08 -0300

    Fix Vercel routing and chunk loading

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Your configuration goes here
-});
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
+})
